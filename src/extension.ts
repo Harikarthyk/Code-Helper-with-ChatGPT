801303629252677f2652f7e6e1fffc6a1bd87919
HariKarthyk <harikarthyk1407@gmail.com>
2023-07-12 18:22:02 +0530

    cleanup .

--- src/extension.ts
+++ src/extension.ts
@@ -487,7 +487,6 @@ export async function activate(context: vscode.ExtensionContext) {
       "codeHelper.refactorWithOpenAI",
       async (document, range) => {
         let token = await getApiFromUser();
-        console.info(token, "1");
         if (!token) {
           return;
         }
